ec8de522dc242130dd2e59b96b5771706bf41213
Shaikh Irfan <shaikhji1998@gmail.com>
2022-11-27 13:40:54 +0530

    New commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,12 @@
 import React, {Component, useState} from "react";
 import '../styles/App.css';
+import Slides from "../data";
 
 const App = () => {
   return (
-    <></>
+    <Slides>
+      
+    </Slides>
   )
 }
 
